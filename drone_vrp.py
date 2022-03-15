45f0825e69eddd101911f83709867d030114df87
colinjkl <colinjiangkelin@gmail.com>
2022-03-15 17:08:25 +0800

    added template classes and test in main.py

--- drone_vrp.py
+++ drone_vrp.py
@@ -136,10 +136,24 @@ class Vehicle(object):
         self.start_node = start_node
         self.speed_factor = speed_factor
         self.item_capacity = item_capacity
+        self.items = item_capacity
         # travel time of the vehicle
         self.travel_turn = 0
         # all the (points, time) including warehouse, customers, or waiting point visited by the vehicle::[(x1,y1,t1), (x2,y2,t2)]
         self.visited_points = [(self.start_node.x, self.start_node.y, 0)] # start from warehouse
+
+
+    def serve_customer(self, customer):
+        '''gives customer quantity of items demanded
+        Args:
+            customer::Customer object
+        '''
+        current_x, current_y, current_t = self.visited_points[len(self.visited_points)-1]
+        assert(self.items > customer.demand)
+        assert(current_x == customer.x)
+        assert(current_y == customer.y)
+        self.items -= customer.demand
+        customer.turn_served = current_t  
         
         
 class Truck(Vehicle):
@@ -159,9 +173,100 @@ class Truck(Vehicle):
         super(Truck, self).__init__(id, start_node, speed_factor, item_capacity)
         self.wait = False
 
+    def find_point_at_t(self, t):
+        '''Returns the (x,y) coordinate at time t
+        Args:
+            t::int
+                the time corresponding to the point you are looking for
+        Returns:
+            (x, y)::tuple(int, int)
+        '''
+        for p in self.visited_points:
+            if p[2] == t:
+                x = p[0]
+                y = p[1]
+                break
+        return (x, y)
+
+    def travel_x(self, point):
+        '''Travel horizontally till self.x matches point.x, and updates attributes
+        Args:
+            point::Point object
+                a point or customer or warehouse destination 
+        '''
+        current_x, current_y, current_t = self.visited_points[len(self.visited_points)-1]
+        while current_x != point.x:
+            current_x += 1 if current_x < point.x else -1
+            current_t += 1
+            self.visited_points.append((current_x, current_y, current_t))
+            self.travel_turn += 1
+
+    def travel_y(self, point):
+        '''Travel vertically till self.y matches point.y, and updates attributes
+        Args:
+            point::Point object
+                a point or customer or warehouse destination
+        '''
+        current_x, current_y, current_t = self.visited_points[len(self.visited_points)-1]
+        while current_y != point.y:
+            current_y += 1 if current_y < point.y else -1
+            current_t += 1
+            self.visited_points.append((current_x, current_y, current_t))
+            self.travel_turn += 1
+
+    def travel_to(self, point, vertical_first):
+        '''Travel to indicated point
+        Args:
+            point::Point object
+                a point or customer or warehouse destination
+            vertical_first::boolean
+                1 to indicate travel_y first then travel_x, 0 otherwise
+        '''
+        if vertical_first:
+            self.travel_y(point)
+            self.travel_x(point)
+        else:
+            self.travel_x(point)
+            self.travel_y(point)
+
+    def charge_to(self, drone, x):
+        '''Charge drone to battery level x
+        Args:
+            drone::drone object
+                the drone to charge
+            x::int
+                target battery level to charge to
+        '''
+        drone.on_truck = True
+        while drone.battery_level < x:
+            drone.battery_level += drone.charging_speed
+            if drone.battery_level > drone.battery_capacity:
+                drone.battery_level = drone.battery_capacity
+            _, _, current_t = drone.visited_points[len(drone.visited_points)-1]
+            px, py = self.find_point_at_t(current_t+1)
+            drone.travel_to(Point(px, py), diagonal_first=True)
+        drone.on_truck = False
+
+    def wait(self, x, t):
+        '''at turn t, wait at current point for x turns
+        Args:
+            x::int
+                the number of turns to wait
+        '''
+        i = len(self.visited_points)-1
+        current_x, current_y, current_t = self.visited_points[i]
+        while current_t != t:
+            self.visited_points[i][2] += x
+            i -= 1
+            current_x, current_y, current_t = self.visited_points[i]                 
+        while x > 0:
+            x -= 1
+            i += 1
+            current_t += 1
+            self.visited_points.insert((current_x, current_y, current_t), i)
 
     def __str__(self):
-        return 'Truck id: {}, start_node: {}, speed_factor: {}, item_capacity: {}, visited_points: {}'.format(self.id, self.start_node.id, self.speed_factor, self.item_capacity, self.visited_points)
+        return 'Truck id: {}, visited_points: {}'.format(self.id, self.visited_points)
         
         
 class Drone(Vehicle):
@@ -192,9 +297,60 @@ class Drone(Vehicle):
         self.battery_level = battery_capacity
         self.on_truck = False
 
+    def travel_straight(self, point):
+        '''Travel vertically or horizontally till self.x or self.y matches point.x or point.y, OR till a direct diagonal path to point can be found
+        Args:
+            point::Point object
+                a point or customer or warehouse destination
+        '''
+        current_x, current_y, current_t = self.visited_points[len(self.visited_points)-1]
+        direct_diag = (abs(point.x-current_x) == abs(point.y-current_y))
+        while not direct_diag:
+            if abs(point.x-current_x) > abs(point.y-current_y):
+                current_x += 1 if current_x < point.x else -1
+            else:
+                current_y += 1 if current_y < point.y else -1     
+            current_t += 1          
+            self.visited_points.append((current_x, current_y, current_t))
+            self.battery_level -= 1 if self.on_truck == False else 0
+            self.travel_turn += 1
+            direct_diag = (abs(point.x-current_x) == abs(point.y-current_y))
+
+    def travel_diag(self, point):
+        '''Travel diagonally till point is reached OR till self.x or self.y matches point.x or point.y
+        Args:
+            point::Point object
+                a point or customer or warehouse destination
+        '''
+        current_x, current_y, current_t = self.visited_points[len(self.visited_points)-1]
+        direct_line = (point.x == current_x or point.y == current_y)
+        while not direct_line:
+            current_x += 1 if current_x < point.x else -1
+            current_y += 1 if current_y < point.y else -1
+            current_t += 1          
+            self.visited_points.append((current_x, current_y, current_t))
+            self.battery_level -= 1 if self.on_truck == False else 0
+            self.travel_turn += 1
+            direct_line = (point.x == current_x or point.y == current_y)
+
+    def travel_to(self, point, diagonal_first):
+        '''Travel to indicated point
+        Args:
+            point::Point object
+                a point or customer or warehouse destination
+            diagonal_first::boolean
+                1 to indicate travel_diag first then travel_straight, 0 otherwise
+        '''
+        if diagonal_first:
+            self.travel_diag(point)
+            self.travel_straight(point)
+        else:
+            self.travel_straight(point)
+            self.travel_diag(point)      
+
     def __str__(self):
-        return 'Drone id: {}, start_node: {}, speed_factor: {}, item_capacity: {}, visited_points: {}, battery_capacity: {}, consumption_rate: {}, charging_speed: {}'\
-            .format(self.id, self.start_node.id, self.speed_factor, self.item_capacity, self.visited_points, self.battery_capacity, self.consumption_rate, self.charging_speed)
+        return 'Drone id: {}, battery_level: {}, visited_points: {}'\
+            .format(self.id, self.battery_level, self.visited_points)
     
         
 class DVRP(object):
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Created on Mon Mar 14 22:30:05 2022
 @author: ivanl
 """
 
+from numpy import diag
 from drone_vrp import *
 
 if __name__ == '__main__':
@@ -12,15 +13,73 @@ if __name__ == '__main__':
     config_file = "config.ini"
     data_type = "DEFAULT"
     
-    # load data and random seed
-    parsed = Parser(config_file, data_type)
+    # # load data and random seed
+    # parsed = Parser(config_file, data_type)
     
-    dvrp = DVRP(parsed.warehouses, parsed.customers, parsed.trucks, parsed.drones)
-    for w in dvrp.warehouses:
-        print(w)
-    for c in dvrp.customers:
-        print(c)
-    for t in dvrp.trucks:
-        print(t)
-    for d in dvrp.drones:
-        print(d)
+    # dvrp = DVRP(parsed.warehouses, parsed.customers, parsed.trucks, parsed.drones)
+    # for w in dvrp.warehouses:
+    #     print(w)
+    # for c in dvrp.customers:
+    #     print(c)
+    # for t in dvrp.trucks:
+    #     print(t)
+    # for d in dvrp.drones:
+    #     print(d)
+
+    warehouse0 = Warehouse(
+        id=0,
+        type=0,
+        x=0,
+        y=0
+    )
+
+    customer0 = Customer(
+        id=0,
+        type=1,
+        x=3,
+        y=3,
+        demand=2
+    )
+    
+    truck0 = Truck(
+        id=0,
+        start_node=warehouse0,
+        speed_factor=1,
+        item_capacity=10
+    )
+
+    drone0 = Drone(
+        id=0,
+        start_node=warehouse0,
+        speed_factor=1,
+        item_capacity=1,
+        battery_capacity=5,
+        consumption_rate=1,
+        charging_speed=5
+    )
+
+    print(truck0)
+    truck0.travel_to(Point(3,3), vertical_first=True)
+    print(truck0)
+
+    print(drone0)
+    drone0.travel_to(Point(2,3), diagonal_first=True)
+    print(drone0)
+    drone0.travel_to(Point(1,3), diagonal_first=True)
+    print(drone0)
+
+
+    # Now truck and drone at same point
+    truck0.charge_to(drone0,5)
+    print(drone0)
+
+    truck0.serve_customer(customer0)
+    print(truck0.items)
+
+    drone0.serve_customer(customer0)
+    print(drone0.items)
+
+
+
+
+
